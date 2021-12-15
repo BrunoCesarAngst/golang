1bb6b6dbaedb176ea66f67d0155f19e93bb7ca63
brunocesarangst <bruno.angst@rede.ulbra.br>
2021-12-15 03:14:10 -0300

    repository

---
+++ useCase/process_transaction/transaction_dto.go
@@ -0,0 +1,12 @@
+package process_transaction
+
+type TransactionDtoInput struct {
+	ID        string
+	AccountID string
+	Amount    float64
+}
+type TransactionDtoOutput struct {
+	ID           string
+	Status       string
+	ErrorMassage string
+}
